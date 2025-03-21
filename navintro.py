5d98deb2a2045d0b1961ead54912d768c7f0b018
mwikali29 <mwikaliredempta871@gmail.com>
2025-03-21 15:56:41 +0300

    assignment week 1

--- navintro.py
+++ navintro.py
@@ -1,10 +1,24 @@
-age = 25
-height = 89.98
-name = "'redempta'"
-condition = "true"
 
-print(type(age))
-print(type(height))
-print(type(name))
-print(type(condition))
+
+ef calculate(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+           return num1 / num2
+        else:
+            return "math error"
+    else:
+        return "invalid operation"
+    
+num1 = float(input("num1:"))
+num2 = float(input("num2:"))
+operation = (input("choose operation:")) 
+
+result = calculate(num1,num2,operation)
+print(f"Result is{result}")
 
