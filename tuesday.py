751b305ac6e5ddf33df571eb2f20f4d7fef092ad
mwikali29 <mwikaliredempta871@gmail.com>
2025-03-21 15:55:54 +0300

    assignment week 1

--- tuesday.py
+++ tuesday.py
@@ -1,8 +1,26 @@
-print("hello world wazaaaa")
+ef calculate(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+           return num1 / num2
+        else:
+            return "math error"
+    else:
+        return "invalid operation"
+    
+num1 = float(input("num1:"))
+num2 = float(input("num2:"))
+operation = (input("choose operation:")) 
 
-name = "ho"
-print(name)
+result = calculate(num1,num2,operation)
+print(f"Result is{result}")
 
-students= ["love","horny","kiki"]
+   
 
-print(students[2])
+
+
